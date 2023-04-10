bccf5ee38cadc8af13f829c2d85c535c35b20e9a
wesleydematos <wesleydematos3@gmail.com>
2023-04-10 10:32:36 -0300

    fix: adjust mail to

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -83,7 +83,7 @@ export const Contacts = () => {
                   target="_blank"
                   href={`mailto=${userData.emailUser}`}
                   onClick={() =>
-                    (window.location.href = "mailto:nekelpatrick.com")
+                    (window.location.href = `mailto:${userData.emailUser}`)
                   }
                 >
                   Send me an email
